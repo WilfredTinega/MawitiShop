f73fac967d72ad44cbe7ffc72271e0fb00d5c887
wilfredtinega <lowkieman@gmail.com>
2025-01-26 11:46:02 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mawiti Shop 
+
+
+# To contribute fork this repo and open PR
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Mawiti Shop Coming Soon...
+    </>
+  )
+}
+
+export default App
