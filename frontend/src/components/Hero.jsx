067da385136e2b2c898a2a40b637896c72d50def
wilfredtinega <lowkieman@gmail.com>
2025-01-28 11:27:18 +0300

    navbar done full responsive

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='flex flex-col sm:flex-row border-gray-400'>
+        {/* Hero left side */}
+        <div className='w-full sm:1-1/2 flex items-center justify-center py-10 sm:py-0'>
+            <div className='flex items-center gap2'>
+                hero
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
